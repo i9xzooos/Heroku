e13994561850d9ad3e8b168264dce0f2b9a01804
i9xzooos <github8wudj@gmail.com>
2025-04-11 19:00:41 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,10 @@ FORBIDDEN="socat nc netcat php lua telnet ncat cryptcat rlwrap msfconsole hydra 
 
 PORT=${PORT:-8080}
 
-keep_alive_local() {
+keep_alive() {
     while true; do
         sleep 30
-        curl -s --max-time 10 "https://$KOYEB_PUBLIC_DOMAIN" >/dev/null
+        curl -s --max-time 10 "https://$RENDER_EXTERNAL_HOSTNAME" >/dev/null || true
     done
 }
 
